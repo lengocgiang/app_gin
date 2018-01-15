101ca396cc4a57e4a64671e1667175a816fa7372
Giang Le Ngoc <gianglengoc.hut@gmail.com>
2018-01-15 17:06:48 +0700

    init

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	// Load html
+	router.LoadHTMLGlob("templates/*")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(
+			// Set the HTTP status to 200(OK)
+			http.StatusOK,
+			// Use the index.html template
+			"index.html",
+			// Pass the data that the page uses(in this case, 'title')
+			gin.H{
+				"title": "Home Page",
+			},
+		)
+	})
+	// Start serving the application
+	router.Run()
+
+}
